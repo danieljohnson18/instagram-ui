cab25d9d73e375d64eedfd7df542f54fa2ca852f
Daniel Arikawe <47419174+danieljohnson18@users.noreply.github.com>
2021-06-29 16:33:29 +0100

    iG-UI

---
+++ src/components/Header/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <nav className="Nav">
+      <div className="Nav-menus">
+        <a className="Nav-brand-logo" href="/">
+          Instagram
+        </a>
+      </div>
+    </nav>
+  );
+};
+
+export default Header;
